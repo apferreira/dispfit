1c1f674359aa68dae5b9f42cf504fcb565d80781
miguel-porto <mpbertolo@gmail.com>
2022-06-30 12:42:49 +0100

    Refactor Rayleigh

--- R/confint.R
+++ R/confint.R
@@ -1,12 +1,17 @@
 confint.dispfit <- function(init.pars, logdistfun, data, lower.limits=c(0, 0), upper.limits=c(100000, 100000), confidence.level) {
+	twopars <- length(init.pars$par) > 1
+	
 	par1 <- init.pars$par[1]
-	par2 <- init.pars$par[2]
 	# parameter estimate standard error
 	par.1.se <- sqrt(diag(solve(numDeriv::hessian(logdistfun, x=init.pars$par, r=data))))[1]
-	par.2.se <- sqrt(diag(solve(numDeriv::hessian(logdistfun, x=init.pars$par, r=data))))[2]
+
+	if(twopars) {
+		par2 <- init.pars$par[2]
+		par.2.se <- sqrt(diag(solve(numDeriv::hessian(logdistfun, x=init.pars$par, r=data))))[2]
+	}
 	
 	# parameter estimate confidence intervals
-	log.dist.ci <- function (a, b, r) {
+	log.dist.ci <- function (a, b=NA, r) {
 		return(logdistfun(par=c(a, b), r))
 	}
 	n.se <- 30
@@ -21,28 +26,34 @@ confint.dispfit <- function(init.pars, logdistfun, data, lower.limits=c(0, 0), u
 	par.1.est <- seq(par.1.ini, par.1.fin, length.out = len)
 
 	par.1.prof <- numeric(len)
-	for (i in 1:len) {
-		if(inherits(lower.limits, "list")) {
-			if(inherits(lower.limits[[2]], "function"))
-				par2.lower <- lower.limits[[2]](c(par.1.est[i], NA), data)
-			else
-				par2.lower <- lower.limits[[2]] + 0.00001
-		} else if(inherits(lower.limits, "numeric")) {
-			par2.lower <- lower.limits[2] + 0.00001
-		}
+	if(twopars) {
+		for (i in 1:len) {
+			if(inherits(lower.limits, "list")) {
+				if(inherits(lower.limits[[2]], "function"))
+					par2.lower <- lower.limits[[2]](c(par.1.est[i], NA), data)
+				else
+					par2.lower <- lower.limits[[2]] + 0.00001
+			} else if(inherits(lower.limits, "numeric")) {
+				par2.lower <- lower.limits[2] + 0.00001
+			}
 
-		if(inherits(upper.limits, "list")) {
-			if(inherits(upper.limits[[2]], "function"))
-				par2.upper <- upper.limits[[2]](c(par.1.est[i], NA), data)
-			else
-				par2.upper <- upper.limits[[2]]
-		} else if(inherits(upper.limits, "numeric")) {
-			par2.upper <- upper.limits[2]
-		}
+			if(inherits(upper.limits, "list")) {
+				if(inherits(upper.limits[[2]], "function"))
+					par2.upper <- upper.limits[[2]](c(par.1.est[i], NA), data)
+				else
+					par2.upper <- upper.limits[[2]]
+			} else if(inherits(upper.limits, "numeric")) {
+				par2.upper <- upper.limits[2]
+			}
 
-		if(par2.upper > par2.lower && is.finite(log.dist.ci(par.1.est[i], par2, data))) {
-			par.1.prof[i] <- optim(log.dist.ci, par = par2, a = par.1.est[i], r = data, lower=par2.lower, upper=par2.upper, method = "Brent")$value
+			if(par2.upper > par2.lower && is.finite(log.dist.ci(par.1.est[i], par2, data))) {
+				par.1.prof[i] <- optim(log.dist.ci, par = par2, a = par.1.est[i], r = data, lower=par2.lower, upper=par2.upper, method = "Brent")$value
+			}
 		}
+	} else {
+		for (i in 1:len) {
+		    par.1.prof[i] = log.dist.ci(a = par.1.est[i], r = data)
+	    }
 	}
 
 	if (length(which(par.1.prof == 0) > 0)) {
@@ -62,55 +73,60 @@ confint.dispfit <- function(init.pars, logdistfun, data, lower.limits=c(0, 0), u
 	}
 	
 
-	# Par 2 CI
-	par.2.ini <- par2 - n.se * par.2.se
-	if (par.2.ini <= lower.limits[2]) {
-		par.2.ini <- lower.limits[2] + 0.01
-	}
-	par.2.fin <- par2 + n.se * par.2.se
-	par.2.est <- seq(par.2.ini , par.2.fin, length.out = len)
+	if(twopars) {
+		# Par 2 CI
+		par.2.ini <- par2 - n.se * par.2.se
+		if (par.2.ini <= lower.limits[2]) {
+			par.2.ini <- lower.limits[2] + 0.01
+		}
+		par.2.fin <- par2 + n.se * par.2.se
+		par.2.est <- seq(par.2.ini , par.2.fin, length.out = len)
 
-	par.2.prof <- numeric(len)
-	for (i in 1:len) {
-		par1.lower <- lower.limits[1] + 0.00001
-		
-		if(inherits(lower.limits, "list")) {
-			if(inherits(lower.limits[[1]], "function"))
-				par1.lower <- lower.limits[[1]](c(NA, par.2.est[i]), data)
-			else
-				par1.lower <- lower.limits[[1]]
-		} else if(inherits(lower.limits, "numeric")) {
-			par1.lower <- lower.limits[1]
+		par.2.prof <- numeric(len)
+		for (i in 1:len) {
+			par1.lower <- lower.limits[1] + 0.00001
+			
+			if(inherits(lower.limits, "list")) {
+				if(inherits(lower.limits[[1]], "function"))
+					par1.lower <- lower.limits[[1]](c(NA, par.2.est[i]), data)
+				else
+					par1.lower <- lower.limits[[1]]
+			} else if(inherits(lower.limits, "numeric")) {
+				par1.lower <- lower.limits[1]
+			}
+
+			if(inherits(upper.limits, "list")) {
+				if(inherits(upper.limits[[1]], "function"))
+					par1.upper <- upper.limits[[1]](c(NA, par.2.est[i]), data)
+				else
+					par1.upper <- upper.limits[[1]]
+			} else if(inherits(upper.limits, "numeric")) {
+				par1.upper <- upper.limits[1]
+			}
+
+			if(par1.upper > par1.lower && is.finite(log.dist.ci(par1, par.2.est[i], data))) {
+				par.2.prof[i] <- optim(log.dist.ci, par = par1, b = par.2.est[i], r = data, lower=par1.lower, upper=par1.upper, method = "Brent")$value
+			}
 		}
 
-		if(inherits(upper.limits, "list")) {
-			if(inherits(upper.limits[[1]], "function"))
-				par1.upper <- upper.limits[[1]](c(NA, par.2.est[i]), data)
-			else
-				par1.upper <- upper.limits[[1]]
-		} else if(inherits(upper.limits, "numeric")) {
-			par1.upper <- upper.limits[1]
+		if (length(which(par.2.prof == 0) > 0)) {
+			par.2.prof <- par.2.prof[-which(par.2.prof == 0)]
 		}
 
-		if(par1.upper > par1.lower && is.finite(log.dist.ci(par1, par.2.est[i], data))) {
-			par.2.prof[i] <- optim(log.dist.ci, par = par1, b = par.2.est[i], r = data, lower=par1.lower, upper=par1.upper, method = "Brent")$value
+		prof.lower <- par.2.prof[1:which.min(par.2.prof)]
+		prof.par.2.lower <- par.2.est[1:which.min(par.2.prof)]
+		par.2.CIlow <- approx(prof.lower, prof.par.2.lower, xout = init.pars$value + qchisq(confidence.level, 1)/2)$y
+
+		if(which.min(par.2.prof) == length(par.2.prof)) {
+			par.2.CIupp <- Inf
+		} else {
+			prof.upper <- par.2.prof[which.min(par.2.prof):length(par.2.prof)]
+			prof.par.2.upper <- par.2.est[which.min(par.2.prof):length(par.2.prof)]
+			par.2.CIupp <- approx(prof.upper, prof.par.2.upper, xout = init.pars$value + qchisq(confidence.level, 1)/2)$y
 		}
-	}
-
-	if (length(which(par.2.prof == 0) > 0)) {
-		par.2.prof <- par.2.prof[-which(par.2.prof == 0)]
-	}
-
-	prof.lower <- par.2.prof[1:which.min(par.2.prof)]
-	prof.par.2.lower <- par.2.est[1:which.min(par.2.prof)]
-	par.2.CIlow <- approx(prof.lower, prof.par.2.lower, xout = init.pars$value + qchisq(confidence.level, 1)/2)$y
-
-	if(which.min(par.2.prof) == length(par.2.prof)) {
-		par.2.CIupp <- Inf
 	} else {
-		prof.upper <- par.2.prof[which.min(par.2.prof):length(par.2.prof)]
-		prof.par.2.upper <- par.2.est[which.min(par.2.prof):length(par.2.prof)]
-		par.2.CIupp <- approx(prof.upper, prof.par.2.upper, xout = init.pars$value + qchisq(confidence.level, 1)/2)$y
+	  par.2.CIlow <- NA
+	  par.2.CIupp <- NA
 	}
 
 	return(c(
--- R/rayleigh.function.R
+++ R/rayleigh.function.R
@@ -1,5 +1,8 @@
 rayleigh.function <- function (data, chi.res.hist, ks.res.hist, confidence.level) {
-  log.dist.rayleigh <- function (r, a) {
+  log.dist.rayleigh <- function (par, r) {
+	a <- par[1]
+	if(a < 0) return(Inf)
+	
     fg <-(1/(pi*a^2)) * exp(-r^2/a^2) ## Rayleigh, as defined in Nathan 2012
     -sum(log(fg)) ## Negative Log Likelihood
   }
@@ -10,7 +13,7 @@ rayleigh.function <- function (data, chi.res.hist, ks.res.hist, confidence.level
   n <- length(data)
   sd0 <- sqrt((n - 1)/n) * sd(data) ## parameter estimate to use as initial value
   # optimization procedure
-  dist.rayleigh.opt <- optim (par = sd0, ## initial value
+  dist.opt <- optim (par = sd0, ## initial value
                               fn = log.dist.rayleigh, ## function to minimize
                               r = data, ## dados
                               method = "Brent", ## one-parameter method
@@ -18,18 +21,18 @@ rayleigh.function <- function (data, chi.res.hist, ks.res.hist, confidence.level
                               upper = 100000)
   # output values
   # AIC
-  aic.rayleigh <- 2 * length(dist.rayleigh.opt$par) + 2 * dist.rayleigh.opt$value
+  aic.rayleigh <- 2 * length(dist.opt$par) + 2 * dist.opt$value
   # AICc
-  aicc.rayleigh <- aic.rayleigh + (2 * length(dist.rayleigh.opt$par)^2 + 2 * length(dist.rayleigh.opt$par))/(length(data) - length(dist.rayleigh.opt$par) - 1 )
+  aicc.rayleigh <- aic.rayleigh + (2 * length(dist.opt$par)^2 + 2 * length(dist.opt$par))/(length(data) - length(dist.opt$par) - 1 )
   # BIC
-  bic.rayleigh <-  2 * dist.rayleigh.opt$value + length(dist.rayleigh.opt$par)*log(length(data))
+  bic.rayleigh <-  2 * dist.opt$value + length(dist.opt$par)*log(length(data))
 
   # Chi-squared - from Press 1992, pp. 621-622
-  chi.expected.values.rayleigh <- dist.rayleigh(chi.res.hist$mids,dist.rayleigh.opt$par)*length(data)*(chi.res.hist$breaks[2] - chi.res.hist$breaks[1])
+  chi.expected.values.rayleigh <- dist.rayleigh(chi.res.hist$mids,dist.opt$par)*length(data)*(chi.res.hist$breaks[2] - chi.res.hist$breaks[1])
   chi.squared.statistic.rayleigh <- sum((chi.res.hist$counts - chi.expected.values.rayleigh)^2 / chi.expected.values.rayleigh)
   chi.squared.pvalue.rayleigh <- 1 - pchisq(chi.squared.statistic.rayleigh, length(chi.res.hist$counts)-2)
   # Kolmogorov-Smirnov - from Sokal 1995, pp. 223-224
-  ks.expected.values.rayleigh <- dist.rayleigh(ks.res.hist$mids,dist.rayleigh.opt$par)*length(data)*(ks.res.hist$breaks[2] - ks.res.hist$breaks[1])
+  ks.expected.values.rayleigh <- dist.rayleigh(ks.res.hist$mids,dist.opt$par)*length(data)*(ks.res.hist$breaks[2] - ks.res.hist$breaks[1])
   simul.rayleigh <- c()
   for (i in seq_along(ks.res.hist$mids)) {
     simul.rayleigh <- c(simul.rayleigh, rep(ks.res.hist$mids[i], round(ks.expected.values.rayleigh[i], 0)))
@@ -38,65 +41,29 @@ rayleigh.function <- function (data, chi.res.hist, ks.res.hist, confidence.level
   ks.d.rayleigh <- as.numeric(ks.rayleigh$statistic)
   ks.p.rayleigh <- as.numeric(ks.rayleigh$p.value)
 
-  # parameter estimate
-  par.1.rayleigh <- dist.rayleigh.opt$par
-  par.2.rayleigh <- NA
-  # parameter estimate standard error
-  par.1.se.rayleigh <- sqrt(diag(solve(numDeriv::hessian(log.dist.rayleigh, x=dist.rayleigh.opt$par, r=data))))
-  # par.1.se.rayleigh <- sd(replicate(1000, optim (par = sd0, ## initial value
-  #                       fn = log.dist.rayleigh, ## function to minimize
-  #                       r = sample(data, replace = T), ## dados
-  #                       method = "Brent", ## one-parameter method
-  #                       lower = 0.000001, ## lower-bound for parameter
-  #                       upper = 100000)$par))
-  par.2.se.rayleigh <- NA
-  # parameter estimate confidence intervals
-  n.se <- 30
-  len <- 1000
-  par.1.ini <- par.1.rayleigh - n.se * par.1.se.rayleigh
-  if (par.1.ini <= 0) {
-    par.1.ini <- 0.01
-  }
-  par.1.fin <- par.1.rayleigh + n.se * par.1.se.rayleigh
-  par.1.est <- seq(par.1.ini , par.1.fin, length.out = len)
-
-  par.1.prof = numeric(1000)
-  for (i in 1:1000) {
-    par.1.prof[i] = log.dist.rayleigh(a = par.1.est[i],
-                                      r = data)
-  }
-
-  prof.lower <- par.1.prof[1:which.min(par.1.prof)]
-  prof.par.1.lower <- par.1.est[1:which.min(par.1.prof)]
-
-  prof.upper <- par.1.prof[which.min(par.1.prof):length(par.1.prof)]
-  prof.par.1.upper <- par.1.est[which.min(par.1.prof):length(par.1.prof)]
-
-  par.1.rayleigh.CIlow <- approx(prof.lower, prof.par.1.lower, xout = dist.rayleigh.opt$value + qchisq(confidence.level, 1)/2)$y
-  par.1.rayleigh.CIupp <- approx(prof.upper, prof.par.1.upper, xout = dist.rayleigh.opt$value + qchisq(confidence.level, 1)/2)$y
-
-  par.2.rayleigh.CIlow <- NA
-  par.2.rayleigh.CIupp <- NA
+  CI <- confint.dispfit(dist.opt, log.dist.rayleigh, data=data, lower=c(0), upper=list(100000), confidence.level=confidence.level)
+  
   # mean
-  mean.rayleigh <- dist.rayleigh.opt$par*sqrt(pi)/2
-  mean.stderr.rayleigh <- msm::deltamethod(~ x1 * sqrt(pi) / 2, mean = dist.rayleigh.opt$par, cov = solve(numDeriv::hessian(log.dist.rayleigh, x=dist.rayleigh.opt$par, r=data)))
+  mean.rayleigh <- dist.opt$par*sqrt(pi)/2
+  mean.stderr.rayleigh <- msm::deltamethod(~ x1 * sqrt(pi) / 2, mean = dist.opt$par, cov = solve(numDeriv::hessian(log.dist.rayleigh, x=dist.opt$par, r=data)))
   # variance
-  variance.rayleigh <- ((4-pi)*(dist.rayleigh.opt$par^2))/4
-  variance.stderr.rayleigh <- msm::deltamethod(~ ((4-pi)*(x1^2))/4, mean = dist.rayleigh.opt$par, cov = solve(numDeriv::hessian(log.dist.rayleigh, x=dist.rayleigh.opt$par, r=data)))
+  variance.rayleigh <- ((4-pi)*(dist.opt$par^2))/4
+  variance.stderr.rayleigh <- msm::deltamethod(~ ((4-pi)*(x1^2))/4, mean = dist.opt$par, cov = solve(numDeriv::hessian(log.dist.rayleigh, x=dist.opt$par, r=data)))
   # standard deviation
-  stdev.rayleigh <- sqrt(((4-pi)*(dist.rayleigh.opt$par^2))/4)
-  stdev.stderr.rayleigh <- msm::deltamethod(~ sqrt(((4-pi)*(x1^2))/4), mean = dist.rayleigh.opt$par, cov = solve(numDeriv::hessian(log.dist.rayleigh, x=dist.rayleigh.opt$par, r=data)))
+  stdev.rayleigh <- sqrt(((4-pi)*(dist.opt$par^2))/4)
+  stdev.stderr.rayleigh <- msm::deltamethod(~ sqrt(((4-pi)*(x1^2))/4), mean = dist.opt$par, cov = solve(numDeriv::hessian(log.dist.rayleigh, x=dist.opt$par, r=data)))
   # skewness
-  skewness.rayleigh <- ((3*sqrt(pi)*dist.rayleigh.opt$par^3)/4)/variance.rayleigh^(3/2)
-  skewness.stderr.rayleigh <- msm::deltamethod(~ (( (3*sqrt(pi)*x1^3) /4)) / (( (4-pi) * (x1^2) )/4)^(3/2), mean = dist.rayleigh.opt$par, cov = solve(numDeriv::hessian(log.dist.rayleigh, x=dist.rayleigh.opt$par, r=data)))
+  skewness.rayleigh <- ((3*sqrt(pi)*dist.opt$par^3)/4)/variance.rayleigh^(3/2)
+  skewness.stderr.rayleigh <- msm::deltamethod(~ (( (3*sqrt(pi)*x1^3) /4)) / (( (4-pi) * (x1^2) )/4)^(3/2), mean = dist.opt$par, cov = solve(numDeriv::hessian(log.dist.rayleigh, x=dist.opt$par, r=data)))
   # kurtosis
-  kurtosis.rayleigh <- (2*dist.rayleigh.opt$par^4)/variance.rayleigh^2
-  kurtosis.stderr.rayleigh <- msm::deltamethod(~ (2*x1^4) / (( (4-pi) * (x1^2) )/4)^(2), mean = dist.rayleigh.opt$par, cov = solve(numDeriv::hessian(log.dist.rayleigh, x=dist.rayleigh.opt$par, r=data)))
+  kurtosis.rayleigh <- (2*dist.opt$par^4)/variance.rayleigh^2
+  kurtosis.stderr.rayleigh <- msm::deltamethod(~ (2*x1^4) / (( (4-pi) * (x1^2) )/4)^(2), mean = dist.opt$par, cov = solve(numDeriv::hessian(log.dist.rayleigh, x=dist.opt$par, r=data)))
   # output
   res <- data.frame(aic.rayleigh, aicc.rayleigh, bic.rayleigh,
                     chi.squared.statistic.rayleigh, chi.squared.pvalue.rayleigh, ks.d.rayleigh, ks.p.rayleigh,
-                    par.1.rayleigh, par.1.rayleigh.CIlow, par.1.rayleigh.CIupp, par.2.rayleigh, par.2.rayleigh.CIlow, par.2.rayleigh.CIupp,
+                    dist.opt$par[1], CI["par1.CIlow"], CI["par1.CIupp"],
+                    dist.opt$par[2], CI["par2.CIlow"], CI["par2.CIupp"],
                     mean.rayleigh, mean.stderr.rayleigh, stdev.rayleigh, stdev.stderr.rayleigh,
                     skewness.rayleigh, skewness.stderr.rayleigh, kurtosis.rayleigh, kurtosis.stderr.rayleigh)
-  rayleigh.values <- list("opt" = dist.rayleigh.opt, "res" = res)
+  rayleigh.values <- list("opt" = dist.opt, "res" = res)
 }
